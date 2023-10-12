b1cbd54fe046679f6619bb81d20ed6464691cb35
rahulkr09a2z <rahulkr123a2z@gmail.com>
2023-10-12 15:53:24 +0530

    [Setup]|1)Devlopment branch created, 2)tailwind install & setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hellow world!</div>;
+  return <div className="text-">Hello Everyone, This is Netflix GPT</div>;
 }
 
 export default App;
